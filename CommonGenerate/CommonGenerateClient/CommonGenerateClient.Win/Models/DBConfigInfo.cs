12cf50c9fc515a01476071d922fa6cc3f9bafe6d
ives <renhanlinbsl@163.com>
2025-04-22 16:52:25 +0800

    DBConfigSingleton

---
+++ CommonGenerate/CommonGenerateClient/CommonGenerateClient.Win/Models/DBConfigInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace CommonGenerateClient.Win.Models
+{
+    public class DBConfigInfo
+    {
+        public string ConfigID { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string ConnectionType { get; set; }
+    }
+}
---
+++ CommonGenerate/CommonGenerateClient/CommonGenerateClient.Win/Models/DBConfigSingleton.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CommonGenerateClient.Win.Models
+{
+    public class DBConfigSingleton
+    {
+        private static readonly object _lock = new object();
+        private static DBConfigSingleton Ins { get; set; }
+        private DBConfigSingleton()
+        {
+
+        }
+
+        #region 
+        public List<DBConfigInfo> DBConfig { get; set; }
+        #endregion
+
+        public static DBConfigSingleton Instance
+        {
+            get
+            {
+                if (Ins != null)
+                {
+                    return Ins;
+                }
+
+                lock (_lock)
+                {
+                    {
+                        if (Ins != null)
+                        {
+                            return Ins;
+                        }
+
+                        var appsettingPath = AppDomain.CurrentDomain.BaseDirectory + "appsetting.json";
+
+                        if (!File.Exists(appsettingPath))
+                        {
+                            throw new Exception("setting err");
+                        }
+
+                        var appsettingContent = File.ReadAllText(appsettingPath);
+                        if (string.IsNullOrEmpty(appsettingContent))
+                        {
+                            throw new Exception("setting cannot parse");
+                        }
+
+                        try
+                        {
+                            Ins = JsonConvert.DeserializeObject<DBConfigSingleton>(appsettingContent);
+                        }
+                        catch (Exception e)
+                        {
+                            throw new Exception("setting cannot parse," + e.Message);
+                        }
+                    }
+                }
+
+                return Ins;
+            }
+        }
+    }
+}
