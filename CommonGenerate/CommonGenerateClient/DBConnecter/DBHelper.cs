baa36995fee75c737e23031c655af893808f94f7
ives <ivesbao@163.com>
2025-08-28 11:56:11 +0800

    FIX c# type err

--- CommonGenerate/CommonGenerateClient/DBConnecter/DBHelper.cs
+++ CommonGenerate/CommonGenerateClient/DBConnecter/DBHelper.cs
@@ -1,4 +1,4 @@
-﻿using CommonGenerateClient.Resource.Dto.Out;
+using CommonGenerateClient.Resource.Dto.Out;
 using SqlSugar;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +19,54 @@ namespace DBConnecter
             });
         }
 
+        public KeyValuePair<string, CSharpDataType> GetDBTypeMapper(DbType dbType, string dbTypeName)
+        {
+            List<KeyValuePair<string, CSharpDataType>> mapperTypes = null;
+            switch (dbType)
+            {
+                case DbType.MySql:
+                    mapperTypes = MySqlDbBind.MappingTypesConst;
+                    break;
+                case DbType.SqlServer:
+                    mapperTypes = SqlServerDbBind.MappingTypesConst;
+                    break;
+                case DbType.Sqlite:
+                    mapperTypes = SqliteDbBind.MappingTypesConst;
+                    break;
+                case DbType.Oracle:
+                    mapperTypes = OracleDbBind.MappingTypesConst;
+                    break;
+                case DbType.PostgreSQL:
+                    mapperTypes = PostgreSQLDbBind.MappingTypesConst;
+                    break;
+                case DbType.Dm:
+                    mapperTypes = DmDbBind.MappingTypesConst;
+                    break;
+                case DbType.Kdbndp:
+                    mapperTypes = KdbndpDbBind.MappingTypesConst;
+                    break;
+                case DbType.Oscar:
+                    mapperTypes = OscarDbBind.MappingTypesConst;
+                    break;
+                case DbType.MySqlConnector:
+                    mapperTypes = MySqlDbBind.MappingTypesConst;
+                    break;
+                case DbType.QuestDB:
+                    mapperTypes = QuestDBDbBind.MappingTypesConst;
+                    break;
+                case DbType.Custom:
+                    throw new System.Exception("自定义数据库类型需要特殊处理");
+                default:
+                    throw new System.Exception("暂不支持该数据库类型");
+            }
+
+            if (mapperTypes == null)
+                throw new System.Exception("数据库类型映射未初始化");
+
+            var typeName = mapperTypes.FirstOrDefault(f => f.Key == dbTypeName);
+            return typeName;
+        }
+
         public IList<UserTabColumnOutDto> GetColumnInfo(string table)
         {
             var fieldList = GetFieldInfoList(table);
@@ -30,12 +78,16 @@ namespace DBConnecter
             var result = new List<UserTabColumnOutDto>();
             foreach (var field in fieldList)
             {
+                //获取实体类型
+                var typeName = GetDBTypeMapper(DBType, field.DataType);
+
                 result.Add(new UserTabColumnOutDto
                 {
                     ColumnName = field.DbColumnName,
                     Comment = field.ColumnDescription,
                     DataDefault = field.DefaultValue,
-                    DataType = field.DataType.ToString(),
+                    //DataType = field.DataType.ToString(),
+                    DataType = typeName.Value.ToString(),
                     ISPrimaryKey = field.IsPrimarykey,
                     Nullable = field.IsNullable ? "Y" : "N"
                 });
